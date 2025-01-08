e9df3f3d5e8ff346d3cf13b50d6754a18df7a937
crb331 <camiloribon31@gmail.com>
2025-01-08 17:14:31 -0500

    fixing redis connection #06

--- src/main.ts
+++ src/main.ts
@@ -40,10 +40,19 @@ async function bootstrap() {
     const origin = req.headers.origin || '*'
     console.log('Cors middleware: Origin:', origin)
 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
     next();
   })
 
